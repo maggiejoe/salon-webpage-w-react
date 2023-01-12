0f9c6ee8840be8aa5af6c92d08b9cbeceff20e24
maggiejoe <maggiejoseph94@gmail.com>
2023-01-11 22:12:48 -0500

    added containers but have not debugged the effect on text

--- my-hair-salon/src/pages/LandingPage/index.js
+++ my-hair-salon/src/pages/LandingPage/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import bgImage from '../../assets/landing-page-img.png';
 
+import { Container, Row, Col } from 'react-bootstrap';
+
 // import { Container } from 'react-bootstrap';
 function LandingPage() {
 
@@ -17,31 +19,44 @@ function LandingPage() {
     const [gradientPosition, setGradientPosition] = useState({ x: 0, y: 0 });
     const [blurAmount, setBlurAmount] = useState(5);
 
-    const handleMouseMove = event => {
-        setGradientPosition({
-            x: event.clientX,
-            y: event.clientY
-        });
-        setBlurAmount(10);
-    };
+    // const handleMouseMove = event => {
+    //     setGradientPosition({
+    //         x: event.clientX,
+    //         y: event.clientY
+    //     });
+    //     setBlurAmount(10);
+    // };
 
-    const handleMouseLeave = () => {
-        setBlurAmount(5);
-    };
+    // const handleMouseLeave = () => {
+    //     setBlurAmount(5);
+    // };
+
     return (
         <div style={styles}>
             <div className="gmContainer"
-                onMouseMove={handleMouseMove}
-                onMouseLeave={handleMouseLeave}
                 style={{
                     '--gradient-x': gradientPosition.x,
                     '--gradient-y': gradientPosition.y,
-                    filter: `blur(${blurAmount}px)`
+                    filter: `blur(${blurAmount}px)`,
+                    position: "relative"
                 }}
             >
-                
+                <div>
+                    <Container>
+                        <Row>
+                            <Col className='m-3 p-3'>
+                                <h1>This is a container</h1>
+                            </Col>
+                            <Col className='m-3 p-3'>
+                                <h1>This is a container</h1>
+                            </Col>
+                            <Col className='m-3 p-3'>
+                                <h1>This is a container</h1>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             </div>
-
         </div>
     )
 }
