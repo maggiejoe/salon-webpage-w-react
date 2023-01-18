cb546263d9b60d0da1f517baa9c9feaa406a8ef0
maggiejoe <maggiejoseph94@gmail.com>
2023-01-17 22:36:37 -0500

    replacing position containers with fexbox

--- my-hair-salon/src/pages/LandingPage/index.js
+++ my-hair-salon/src/pages/LandingPage/index.js
@@ -1,26 +1,45 @@
 import React from 'react';
 
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function LandingPage() {
 
-    const bgStyles = {
-        backgroundSize: 'contain',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        height: '80vh',
-        width: '100%'
-    }
-
     return (
-        <div className='landingPageContainer' style={{ width: '100%' }}>
-            <div className='background-img' style={bgStyles}>
-                <Container>
-                    <Row>
-                        <h1 className='d-flex justify-content-center align-items-center'>Container Header</h1>
-                    </Row>
-                </Container>
-            </div>
+        <div className='background-img'>
+            <Container>
+                <Row className=''>
+                    <Col className='d-flex justify-content-center mt-4'>
+                        <div className='landingPage-ContainerBg p-3 rounded shadow-lg'>
+                            <h1 className='text-forward'>About Us</h1>
+                            <p>
+                                Ex aute excepteur aliquip amet esse incididunt nulla eu exercitation quis ullamco ea consequat. Exercitation dolore adipisicing veniam et. Commodo aute reprehenderit cupidatat ex ex voluptate adipisicing dolor eu. Sint sit nisi nulla aliquip culpa do sint. Ut sunt dolore nulla id esse adipisicing est adipisicing occaecat exercitation. Reprehenderit ea est et deserunt ex sunt.
+                            </p>
+                        </div>
+                    </Col>
+                    <Col className='d-flex justify-content-center'>
+                        <div className='border'>
+                            <h1 className=''>Container header</h1>
+                            <p>lorem Ipsum</p>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className='d-flex justify-content-center mt-5'>
+                        <div className='border'>
+                            <h1 className=''>Our Vision</h1>
+                            <p>
+                                Ex amet consequat officia elit deserunt sunt. Ut do amet in adipisicing labore aliqua cupidatat fugiat Lorem. Et id qui Lorem aliquip ex enim Lorem adipisicing cupidatat aliquip dolore voluptate adipisicing. Minim nisi culpa pariatur anim et proident cillum pariatur in fugiat pariatur. Officia ex esse tempor magna irure nostrud qui. Incididunt enim occaecat id laboris occaecat dolor in magna occaecat amet nisi nulla duis. Non et ullamco veniam laboris adipisicing aliquip proident dolor.
+                            </p>
+                        </div>
+                    </Col>
+                    <Col className='d-flex justify-content-center'>
+                        <div className='border'>
+                            <h1 className=''>Container header</h1>
+                            <p>lorem Ipsum</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
