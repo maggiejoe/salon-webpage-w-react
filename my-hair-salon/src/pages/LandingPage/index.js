90055c6505a440c6604974a67f537d483b45db10
maggiejoe <maggiejoseph94@gmail.com>
2023-01-17 22:41:10 -0500

    adjusted background opacity to have clear text

--- my-hair-salon/src/pages/LandingPage/index.js
+++ my-hair-salon/src/pages/LandingPage/index.js
@@ -10,7 +10,7 @@ function LandingPage() {
                 <Row className=''>
                     <Col className='d-flex justify-content-center mt-4'>
                         <div className='landingPage-ContainerBg p-3 rounded shadow-lg'>
-                            <h1 className='text-forward'>About Us</h1>
+                            <h1 className=''>About Us</h1>
                             <p>
                                 Ex aute excepteur aliquip amet esse incididunt nulla eu exercitation quis ullamco ea consequat. Exercitation dolore adipisicing veniam et. Commodo aute reprehenderit cupidatat ex ex voluptate adipisicing dolor eu. Sint sit nisi nulla aliquip culpa do sint. Ut sunt dolore nulla id esse adipisicing est adipisicing occaecat exercitation. Reprehenderit ea est et deserunt ex sunt.
                             </p>
@@ -25,7 +25,7 @@ function LandingPage() {
                 </Row>
                 <Row>
                     <Col className='d-flex justify-content-center mt-5'>
-                        <div className='border'>
+                        <div className='landingPage-ContainerBg p-3 rounded shadow-lg'>
                             <h1 className=''>Our Vision</h1>
                             <p>
                                 Ex amet consequat officia elit deserunt sunt. Ut do amet in adipisicing labore aliqua cupidatat fugiat Lorem. Et id qui Lorem aliquip ex enim Lorem adipisicing cupidatat aliquip dolore voluptate adipisicing. Minim nisi culpa pariatur anim et proident cillum pariatur in fugiat pariatur. Officia ex esse tempor magna irure nostrud qui. Incididunt enim occaecat id laboris occaecat dolor in magna occaecat amet nisi nulla duis. Non et ullamco veniam laboris adipisicing aliquip proident dolor.
