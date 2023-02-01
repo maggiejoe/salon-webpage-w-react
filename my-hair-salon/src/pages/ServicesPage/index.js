4e8b1021941aa9462855ea38388712a122a1911b
maggiejoe <maggiejoseph94@gmail.com>
2023-01-31 22:58:52 -0500

    updated text-decoration-none to list-unstyled

--- my-hair-salon/src/pages/ServicesPage/index.js
+++ my-hair-salon/src/pages/ServicesPage/index.js
@@ -9,41 +9,41 @@ function ServicesPage() {
                     <Col className="d-flex col-3">
                         <div className="blurred-container-bg p-3 w-100">
                             <h1 className='d-flex justify-content-center fs-3'>Cut's & Washes</h1>
-                            <ul className="">
-                                <li className="text-deco">Wash, Cut & Style</li>
-                                <li>Children Wash, Cut & style {'('}10 and under{')'}</li>
+                            <ul className="list-unstyled">
+                                <li className="d-flex justify-content-center">Wash, Cut & Style</li>
+                                <li className="d-flex justify-content-center">Children Wash, Cut & Style {'('}10 and under{')'}</li>
                             </ul>
                         </div>
                     </Col>
                     <Col className="d-flex col-3">
                         <div className="blurred-container-bg p-3 w-100">
                             <h1 className='d-flex justify-content-center fs-3'>Up-do's & Styles</h1>
-                            <ul>
-                                <li>Wash and Blowdry</li>
-                                <li>Blow Out & Style</li>
-                                <li>Up-do's</li>
-                                <li>Children's Up-do's</li>
+                            <ul className="list-unstyled">
+                                <li className="d-flex justify-content-center">Wash and Blowdry</li>
+                                <li className="d-flex justify-content-center">Blow Out & Style</li>
+                                <li className="d-flex justify-content-center">Up-do's</li>
+                                <li className="d-flex justify-content-center">Children's Up-do's</li>
                             </ul>
                         </div>
                     </Col>
                     <Col className="d-flex col-3">
                         <div className="blurred-container-bg p-3 w-100">
                             <h1 className='d-flex justify-content-center fs-3'>Color & Highlights</h1>
-                            <ul>
-                                <li>Roots & Blowdry</li>
-                                <li>Roots, Blowdry & Cut</li>
-                                <li>Full & Partial Highlights, Blowdry, Cut</li>
-                                <li>Full & Partial Balyage</li>
-                                <li>Adding Toner</li>
+                            <ul className="list-unstyled">
+                                <li className="d-flex justify-content-center">Roots & Blowdry</li>
+                                <li className="d-flex justify-content-center">Roots, Blowdry & Cut</li>
+                                <li className="d-flex justify-content-center">Full & Partial Highlights, Blowdry</li>
+                                <li className="d-flex justify-content-center">Full & Partial Balyage</li>
+                                <li className="d-flex justify-content-center">Adding Toner</li>
                             </ul>
                         </div>
                     </Col>
                     <Col className="d-flex col-3">
                         <div className="blurred-container-bg p-3 w-100">
                             <h1 className='d-flex justify-content-center fs-3'>Chemical Treatments</h1>
-                            <ul>
-                                <li>Perm</li>
-                                <li>Keratin Treatment</li>
+                            <ul className="list-unstyled">
+                                <li className="d-flex justify-content-center">Perm</li>
+                                <li className="d-flex justify-content-center">Keratin Treatment</li>
                             </ul>
                         </div>
                     </Col>
