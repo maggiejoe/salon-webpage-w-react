32c3cb67e865020f07a11a4b16109bbff52db95d
maggiejoe <maggiejoseph94@gmail.com>
2023-01-31 22:34:22 -0500

    adjusted services page to reflect hair salon servies

--- my-hair-salon/src/pages/ServicesPage/index.js
+++ my-hair-salon/src/pages/ServicesPage/index.js
@@ -5,23 +5,30 @@ function ServicesPage() {
     return (
         <div className="body-content services-background-img d-flex align-items-center">
             <Container className="">
-                <Row className="d-flex">
-                    <Col className="">
-                        <div className="blurred-container-bg">
-                            <h1 className="d-flex justify-content-center">Hair Services</h1>
-                            <h2 className='fs-4'>Cut's & Washes</h2>
-                            <ul>
+                <Row className="">
+                    <Col className="d-flex col-3">
+                        <div className="blurred-container-bg p-3">
+                            <h1 className='d-flex justify-content-center fs-3'>Cut's & Washes</h1>
+                            <ul className="text-decoration-none">
                                 <li>Wash, Cut & Style</li>
                                 <li>Children Wash, Cut & style {'('}10 and under{')'}</li>
                             </ul>
-                            <h2 className='fs-4'>Up-do's & Styles</h2>
+                        </div>
+                    </Col>
+                    <Col className="d-flex col-3">
+                        <div className="blurred-container-bg p-3">
+                            <h1 className='d-flex justify-content-center fs-3'>Up-do's & Styles</h1>
                             <ul>
                                 <li>Wash and Blowdry</li>
                                 <li>Blow Out & Style</li>
                                 <li>Up-do's</li>
                                 <li>Children's Up-do's</li>
                             </ul>
-                            <h2 className='fs-4'>Color & Highlights</h2>
+                        </div>
+                    </Col>
+                    <Col className="d-flex col-3">
+                        <div className="blurred-container-bg p-3">
+                            <h1 className='d-flex justify-content-center fs-3'>Color & Highlights</h1>
                             <ul>
                                 <li>Roots & Blowdry</li>
                                 <li>Roots, Blowdry & Cut</li>
@@ -29,25 +36,15 @@ function ServicesPage() {
                                 <li>Full & Partial Balyage</li>
                                 <li>Adding Toner</li>
                             </ul>
-                            <h2 className='fs-4'>Chemical Treatments</h2>
+                        </div>
+                    </Col>
+                    <Col className="d-flex col-3">
+                        <div className="blurred-container-bg p-3">
+                            <h1 className='d-flex justify-content-center fs-3'>Chemical Treatments</h1>
                             <ul>
                                 <li>Perm</li>
                                 <li>Keratin Treatment</li>
                             </ul>
-                            <h2 className='fs-4'>Extentions</h2>
-                            <ul>
-                                <li>Contact for Pricing</li>
-                            </ul>
-                        </div>
-                    </Col>
-                    <Col className="">
-                        <div className="blurred-container-bg d-flex justify-content-center">
-                            <h1>Skincare Services</h1>
-                        </div>
-                    </Col>
-                    <Col className="">
-                        <div className="blurred-container-bg d-flex justify-content-center">
-                            <h1>Waxing</h1>
                         </div>
                     </Col>
                 </Row>
